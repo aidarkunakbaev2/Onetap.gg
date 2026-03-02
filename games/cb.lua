d36fa5f1112a958051f54cb95824e08ee9da2001
aidarkunakbaev2 <kunakbaevaidar26@gmail.com>
2026-03-02 18:42:26 +0500

    Update cb.lua

--- games/cb.lua
+++ games/cb.lua
@@ -1,14 +1,14 @@
 --[[
-                      __                                ___.    
+                      __                                ___   
   ____   ____   _____/  |______  ______     ______  __ _\_ |__  
  /  _ \ /    \_/ __ \   __\__  \ \____ \    \____ \|  |  \ __ \ 
 (  |_| )   |  \  ___/|  |  / __ \|  |_> >   |  |_> >  |  / \_\ \
- \____/|___|  /\___  >__| (____  /   __/ /|\ |   __/|____/|_____/
-            \/     \/          \/|__|    \|/ |__|              Art by deportal
+ \____/|___|  /\___  >__| (____  /   __/ /\ |   __/|____/|_____/
+            \/     \/          \/|__|    \/ |__|              Art by deportal
 
 
 ]]
 
 
 
-game.Players.LocalPlayer:Kick("URGENT PSA - API for cb disabled. Reason: WebHooks+Platoboost")
+game.Players.LocalPlayer:Kick("URGENT PSA - API ONLY for cb disabled. Reason: Someone put stealer in API")
